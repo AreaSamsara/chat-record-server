72b144c1759f633d7a29e0f14ac6afb7424fefa5
AreaSamsara <AreaSamsara@outlook.com>
2025-11-06 17:14:44 +0800

    引入spdlog包

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <string>
-
+#include <spdlog/spdlog.h>
 #include "server/server.hpp"
+
+// 初始化日志管理器
+void init_logger()
+{
+    // 设置日志等级
+    spdlog::set_level(spdlog::level::debug);
+    // 设置日志格式
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
+
+    spdlog::info("Logger initialized as level {}",
+                 spdlog::level::to_string_view(spdlog::get_level()));
+}
 
 int main()
 {
     std::string name = "AS";
-    std::cout << std::format("Hello world, {}!", name) << std::endl;
+    std::cout << std::format("Hello, {}!", name) << std::endl;
+
+    init_logger();
+
     AreaSamsara::server::run_server();
 
     return 0;
