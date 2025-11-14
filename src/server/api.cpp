54c40b32932dfa3203ac6cb75d0dcc8c107e5c7e
AreaSamsara <AreaSamsara@outlook.com>
2025-11-14 22:08:39 +0800

    初始化用户注册api

--- src/server/api.cpp
+++ src/server/api.cpp
@@ -9,6 +9,7 @@
 
 namespace AreaSamsara::server
 {
+    // GET Hello
     void hello_handler(const httplib::Request &req, httplib::Response &rsp)
     {
         auto name = req.get_param_value("name");
@@ -18,22 +19,47 @@ namespace AreaSamsara::server
         rsp.set_content(response_content, "text/plain");
     }
 
+    std::map<std::string, HttpHandler> get_handlers = {
+        {"/Hello", hello_handler}};
+
+    // POST Echo
     void echo_handler(const httplib::Request &req, httplib::Response &rsp)
     {
         spdlog::info("POST /Echo -> receive post data: {}", req.body);
 
-        Json::Value response_data;
-        response_data["status"] = "success";
-        response_data["message"] = std::format("Data received: {}", req.body);
+        Json::Value response_json;
+        response_json["status"] = "success";
+        response_json["message"] = std::format("Data received: {}", req.body);
 
         Json::StreamWriterBuilder writer;
         writer.settings_["emitUTF8"] = true;
-        rsp.set_content(Json::writeString(writer, response_data), "application/json");
+        rsp.set_content(Json::writeString(writer, response_json), "application/json");
     }
 
-    std::map<std::string, HttpHandler> get_handlers = {
-        {"/Hello", hello_handler}};
+    // POST SignUp
+    void sign_up_handler(const httplib::Request &req, httplib::Response &rsp)
+    {
+        // spdlog::info("POST /SignUp -> receive post data: {}", req.body);
+
+        // Json::Value request_json;
+        // std::istringstream stream(req.body);
+        // std::string errors;
+        // Json::parseFromStream(Json::CharReaderBuilder(), stream, &request_json, &errors);
+
+        // std::string user_name = request_json["user_name"].asString();
+        // std::string email = request_json["email"].asString();
+        // std::string phone = request_json["phone"].asString();
+
+        // Json::Value response_json;
+        // response_json["status"] = "success";
+        // response_json["message"] = std::format("Data received: {}", req.body);
+
+        // Json::StreamWriterBuilder writer;
+        // writer.settings_["emitUTF8"] = true;
+        // rsp.set_content(Json::writeString(writer, response_json), "application/json");
+    }
 
     std::map<std::string, HttpHandler> post_handlers = {
-        {"/Echo", echo_handler}};
+        {"/Echo", echo_handler},
+        {"/SignUp", sign_up_handler}};
 }
